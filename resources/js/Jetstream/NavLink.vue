bdfa61eb1568f85dc402c46e0a40d09ba33fac9f
Philipp Reinking <philipp@deck9.co>
2021-09-20 19:16:06 +0200

    change app layout

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -1,7 +1,7 @@
 <template>
-    <Link :href="href" :class="classes">
-        <slot />
-    </Link>
+  <Link :href="href" :class="classes">
+    <slot />
+  </Link>
 </template>
 
 <script>
@@ -17,8 +17,8 @@ export default defineComponent({
   computed: {
     classes() {
       return this.active
-        ? "inline-flex items-center px-1 pt-1 border-b-2 border-blue-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition"
-        : "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition";
+        ? "inline-flex items-center px-1 text-sm font-bold text-grey-100 focus:outline-none transition"
+        : "inline-flex items-center px-1 text-sm font-medium text-grey-400 hover:text-grey-100 focus:outline-none focus:text-grey-200 transition";
     },
   },
 });
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -5,7 +5,7 @@
     <jet-banner />
 
     <div class="min-h-screen bg-gray-100">
-      <nav class="bg-white border-b border-gray-100">
+      <nav class="bg-grey-800">
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between h-16">
@@ -13,12 +13,12 @@
               <!-- Logo -->
               <div class="flex-shrink-0 flex items-center">
                 <Link :href="route('dashboard')">
-                  <jet-application-mark class="block h-9 w-auto" />
+                  <jet-application-logo mode="dark" class="block h-5 w-auto" />
                 </Link>
               </div>
 
               <!-- Navigation Links -->
-              <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+              <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 <jet-nav-link
                   :href="route('dashboard')"
                   :active="route().current('dashboard')"
@@ -28,7 +28,7 @@
               </div>
             </div>
 
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="flex sm:items-center sm:ml-6">
               <div class="ml-3 relative">
                 <!-- Teams Dropdown -->
                 <jet-dropdown
@@ -176,9 +176,9 @@
                           leading-4
                           font-medium
                           rounded-md
-                          text-gray-500
-                          bg-white
-                          hover:text-gray-700
+                          text-grey-300
+                          bg-grey-900
+                          hover:text-blue-200
                           focus:outline-none
                           transition
                         "
@@ -230,188 +230,9 @@
                 </jet-dropdown>
               </div>
             </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-              <button
-                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                class="
-                  inline-flex
-                  items-center
-                  justify-center
-                  p-2
-                  rounded-md
-                  text-gray-400
-                  hover:text-gray-500 hover:bg-gray-100
-                  focus:outline-none focus:bg-gray-100 focus:text-gray-500
-                  transition
-                "
-              >
-                <svg
-                  class="h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    :class="{
-                      hidden: showingNavigationDropdown,
-                      'inline-flex': !showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  <path
-                    :class="{
-                      hidden: !showingNavigationDropdown,
-                      'inline-flex': showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <!-- Responsive Navigation Menu -->
-        <div
-          :class="{
-            block: showingNavigationDropdown,
-            hidden: !showingNavigationDropdown,
-          }"
-          class="sm:hidden"
-        >
-          <div class="pt-2 pb-3 space-y-1">
-            <jet-responsive-nav-link
-              :href="route('dashboard')"
-              :active="route().current('dashboard')"
-            >
-              Dashboard
-            </jet-responsive-nav-link>
-          </div>
-
-          <!-- Responsive Settings Options -->
-          <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="flex items-center px-4">
-              <div
-                v-if="$page.props.jetstream.managesProfilePhotos"
-                class="flex-shrink-0 mr-3"
-              >
-                <img
-                  class="h-10 w-10 rounded-full object-cover"
-                  :src="$page.props.user.profile_photo_url"
-                  :alt="$page.props.user.name"
-                />
-              </div>
-
-              <div>
-                <div class="font-medium text-base text-gray-800">
-                  {{ $page.props.user.name }}
-                </div>
-                <div class="font-medium text-sm text-gray-500">
-                  {{ $page.props.user.email }}
-                </div>
-              </div>
-            </div>
-
-            <div class="mt-3 space-y-1">
-              <jet-responsive-nav-link
-                :href="route('profile.show')"
-                :active="route().current('profile.show')"
-              >
-                Profile
-              </jet-responsive-nav-link>
-
-              <jet-responsive-nav-link
-                :href="route('api-tokens.index')"
-                :active="route().current('api-tokens.index')"
-                v-if="$page.props.jetstream.hasApiFeatures"
-              >
-                API Tokens
-              </jet-responsive-nav-link>
-
-              <!-- Authentication -->
-              <form method="POST" @submit.prevent="logout">
-                <jet-responsive-nav-link as="button">
-                  Log Out
-                </jet-responsive-nav-link>
-              </form>
-
-              <!-- Team Management -->
-              <template v-if="$page.props.jetstream.hasTeamFeatures">
-                <div class="border-t border-gray-200"></div>
-
-                <div class="block px-4 py-2 text-xs text-gray-400">
-                  Manage Team
-                </div>
-
-                <!-- Team Settings -->
-                <jet-responsive-nav-link
-                  :href="route('teams.show', $page.props.user.current_team)"
-                  :active="route().current('teams.show')"
-                >
-                  Team Settings
-                </jet-responsive-nav-link>
-
-                <jet-responsive-nav-link
-                  :href="route('teams.create')"
-                  :active="route().current('teams.create')"
-                  v-if="$page.props.jetstream.canCreateTeams"
-                >
-                  Create New Team
-                </jet-responsive-nav-link>
-
-                <div class="border-t border-gray-200"></div>
-
-                <!-- Team Switcher -->
-                <div class="block px-4 py-2 text-xs text-gray-400">
-                  Switch Teams
-                </div>
-
-                <template
-                  v-for="team in $page.props.user.all_teams"
-                  :key="team.id"
-                >
-                  <form @submit.prevent="switchToTeam(team)">
-                    <jet-responsive-nav-link as="button">
-                      <div class="flex items-center">
-                        <svg
-                          v-if="team.id == $page.props.user.current_team_id"
-                          class="mr-2 h-5 w-5 text-green-400"
-                          fill="none"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                          ></path>
-                        </svg>
-                        <div>{{ team.name }}</div>
-                      </div>
-                    </jet-responsive-nav-link>
-                  </form>
-                </template>
-              </template>
-            </div>
           </div>
         </div>
       </nav>
-
-      <!-- Page Heading -->
-      <header class="bg-white shadow" v-if="$slots.header">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <slot name="header"></slot>
-        </div>
-      </header>
 
       <!-- Page Content -->
       <main>
@@ -423,7 +244,7 @@
 
 <script>
 import { defineComponent } from "vue";
-import JetApplicationMark from "@/Jetstream/ApplicationMark.vue";
+import JetApplicationLogo from "@/Jetstream/ApplicationLogo.vue";
 import JetBanner from "@/Jetstream/Banner.vue";
 import JetDropdown from "@/Jetstream/Dropdown.vue";
 import JetDropdownLink from "@/Jetstream/DropdownLink.vue";
@@ -438,7 +259,7 @@ export default defineComponent({
 
   components: {
     Head,
-    JetApplicationMark,
+    JetApplicationLogo,
     JetBanner,
     JetDropdown,
     JetDropdownLink,
