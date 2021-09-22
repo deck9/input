c9b85fdb333be943f3be7a513fd18f449954c9d7
Philipp Reinking <philipp@deck9.co>
2021-09-23 01:29:43 +0200

    enable typescript build

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+export default {
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "@vue/prettier",
+        "@vue/prettier/@typescript-eslint",
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: "@typescript-eslint/parser",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    },
+    globals: {
+        require: "readonly",
+        route: "readonly",
+        defineEmits: "readonly",
+        defineProps: "readonly",
+    },
+};
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,17 +1,23 @@
-require("./bootstrap");
-
-import { createApp, h } from "vue";
+import { App, createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import type { Config, RouteParamsWithQueryOverload } from "ziggy-js";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+
+declare function route(
+    name: string,
+    params?: RouteParamsWithQueryOverload,
+    absolute?: boolean,
+    config?: Config,
+): string;
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
+        createApp({ render: () => h(app, props) })
             .use(plugin)
             .mixin({ methods: { route } })
             .mount(el);
