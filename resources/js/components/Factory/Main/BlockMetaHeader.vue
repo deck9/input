7e603471a0ed65c5dd8ef06a7d5ee4b7be0e72fa
Philipp Reinking <philipp@deck9.co>
2021-10-26 13:27:17 +0200

    change eslint config and use locale date string on meta header

--- resources/js/components/Factory/Main/BlockMetaHeader.vue
+++ resources/js/components/Factory/Main/BlockMetaHeader.vue
@@ -2,21 +2,27 @@
   <div class="mb-4 text-grey-500 text-xs font-mono flex justify-between">
     <div>
       <span class="text-xs mr-1">ID</span>
-      <span class="bg-grey-300 inline-block leading-normal px-2 rounded">{{ block.uuid }}</span>
+      <span class="bg-grey-300 inline-block leading-normal px-2 rounded">
+        {{ block.uuid }}
+      </span>
     </div>
     <div>
       <span class="text-xs mr-1">Last Change</span>
-      <span class="bg-grey-300 inline-block leading-normal px-2 rounded">
-        {{
-          block.updated_at
-        }}
-      </span>
+      <span class="bg-grey-300 inline-block leading-normal px-2 rounded">{{
+        formattedUpdatedAt
+      }}</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  block: FormBlockModel
-}>()
+import { computed } from "vue";
+
+const props = defineProps<{
+  block: FormBlockModel;
+}>();
+
+const formattedUpdatedAt = computed(() => {
+  return new Date(props.block?.updated_at).toLocaleString();
+});
 </script>
