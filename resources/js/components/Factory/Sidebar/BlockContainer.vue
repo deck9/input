87fa1814d481f08cf20f734acd10d3e69158b5c3
Philipp Reinking <philipp@deck9.co>
2022-03-05 16:36:12 +0100

    add animation to storyboard blocks

--- resources/js/components/Factory/Sidebar/BlockContainer.vue
+++ resources/js/components/Factory/Sidebar/BlockContainer.vue
@@ -1,9 +1,11 @@
 <template>
   <Container lock-axis="y" orientation="vertical" @drop="onDrop">
-    <Draggable v-for="block in store.blocks" :key="block.uuid">
-      <Block :block="block" :key="`${block.sequence}-${block.uuid}`" />
-    </Draggable>
-    <FinalBlock />
+    <TransitionGroup v-bind="transitionClasses">
+      <Draggable v-for="block in store.blocks" :key="block.uuid">
+        <Block :block="block" :key="`${block.sequence}-${block.uuid}`" />
+      </Draggable>
+      <FinalBlock key="final-block" />
+    </TransitionGroup>
   </Container>
 </template>
 
@@ -11,15 +13,34 @@
 import Block from "./Block.vue";
 import FinalBlock from "./FinalBlock.vue";
 import { Container, Draggable } from "vue3-smooth-dnd";
+import { ref, computed } from "vue";
 import { useForm } from "@/stores";
+import { nextTick } from "process";
 
 const store = useForm();
+const enableCssTransition = ref(true);
+
+const transitionClasses = computed((): Record<string, string> => {
+  if (enableCssTransition.value) {
+    return {
+      moveClass: "transition duration-200",
+      leaveToClass: "opacity-0 -translate-y-2",
+      leaveActiveClass: "absolute inset-x-0 transition duration-150 ease-out",
+    };
+  }
+
+  return {};
+});
 
 const onDrop = ({ removedIndex, addedIndex }: any): void => {
+  enableCssTransition.value = false;
   if (removedIndex === null || addedIndex === null) {
     return;
   }
 
   store.changeBlockSequence(removedIndex, addedIndex);
+  nextTick(() => {
+    enableCssTransition.value = true;
+  });
 };
 </script>
