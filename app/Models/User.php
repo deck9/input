30cc0b7b7158cd755c0571c58afd92703274733e
Philipp Reinking <philipp@deck9.co>
2022-03-12 19:25:26 +0100

    update missing auth pages

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Laravel\Sanctum\HasApiTokens;
 use Laravel\Jetstream\HasProfilePhoto;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -3,15 +3,19 @@
 
   <jet-authentication-card>
     <template #logo>
-      <jet-application-logo class="h-8 mx-auto" />
+      <jet-application-logo mode="dark" class="mx-auto h-8" />
     </template>
+
+    <h1 class="mb-6 text-center text-2xl font-bold text-grey-300">
+      Set a new password
+    </h1>
 
     <jet-validation-errors class="mb-4" />
 
     <form @submit.prevent="submit">
       <div>
-        <jet-label for="email" value="Email" />
-        <jet-input
+        <d9-label for="email" label="Email" />
+        <d9-input
           id="email"
           type="email"
           class="mt-1 block w-full"
@@ -22,8 +26,8 @@
       </div>
 
       <div class="mt-4">
-        <jet-label for="password" value="Password" />
-        <jet-input
+        <d9-label for="password" label="Password" />
+        <d9-input
           id="password"
           type="password"
           class="mt-1 block w-full"
@@ -34,8 +38,8 @@
       </div>
 
       <div class="mt-4">
-        <jet-label for="password_confirmation" value="Confirm Password" />
-        <jet-input
+        <d9-label for="password_confirmation" label="Confirm Password" />
+        <d9-input
           id="password_confirmation"
           type="password"
           class="mt-1 block w-full"
@@ -45,13 +49,14 @@
         />
       </div>
 
-      <div class="flex items-center justify-end mt-4">
-        <jet-button
-          :class="{ 'opacity-25': form.processing }"
+      <div class="mt-4 flex items-center justify-end">
+        <d9-button
+          type="submit"
+          color="dark"
+          :isLoading="form.processing"
           :disabled="form.processing"
-        >
-          Reset Password
-        </jet-button>
+          label="Reset Password"
+        />
       </div>
     </form>
   </jet-authentication-card>
@@ -62,9 +67,7 @@ import { defineComponent } from "vue";
 import { Head } from "@inertiajs/inertia-vue3";
 import JetAuthenticationCard from "@/Jetstream/AuthenticationCard.vue";
 import JetApplicationLogo from "@/Jetstream/ApplicationLogo.vue";
-import JetButton from "@/Jetstream/Button.vue";
-import JetInput from "@/Jetstream/Input.vue";
-import JetLabel from "@/Jetstream/Label.vue";
+import { D9Button, D9Input, D9Label } from "@deck9/ui";
 import JetValidationErrors from "@/Jetstream/ValidationErrors.vue";
 
 export default defineComponent({
@@ -72,9 +75,9 @@ export default defineComponent({
     Head,
     JetAuthenticationCard,
     JetApplicationLogo,
-    JetButton,
-    JetInput,
-    JetLabel,
+    D9Button,
+    D9Input,
+    D9Label,
     JetValidationErrors,
   },
 
