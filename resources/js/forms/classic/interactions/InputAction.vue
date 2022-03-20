cbfeefbe786dd15da80223e7248b5019e5ede95a
Philipp Reinking <philipp@deck9.co>
2022-03-21 00:11:29 +0100

    style form input

--- resources/js/forms/classic/interactions/InputAction.vue
+++ resources/js/forms/classic/interactions/InputAction.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <label class="block" :for="action.id"> {{ action.label }}</label>
+    <label class="sr-only block" :for="action.id"> {{ action.label }}</label>
     <input
       type="text"
+      class="block w-full max-w-xs rounded border border-grey-300 focus:border-grey-600 focus:outline-none focus:ring-1 focus:ring-grey-600"
       :name="block.id"
       :id="action.id"
       :placeholder="action.label || 'Enter text'"
