0bb57ca563682fecc14093d8621d99709b633482
Philipp Reinking <philipp@deck9.co>
2022-03-23 21:03:33 +0100

    add first draft of navigator arrows

---
+++ resources/js/forms/classic/Navigator.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="grid w-24 grid-cols-4 rounded border border-grey-200 text-grey-800"
+  >
+    <button
+      type="button"
+      class="border-r border-grey-100 py-1 hover:bg-grey-100"
+      @click="emit('prev')"
+    >
+      <D9Icon size="md" icon="chevron-left" />
+    </button>
+    <span class="col-span-2 inline-block whitespace-nowrap py-1 font-medium">
+      <span>{{ currentPage }}</span>
+      <span v-if="totalPages"> / {{ totalPages }}</span>
+    </span>
+    <button
+      type="button"
+      class="border-l border-grey-100 py-1 hover:bg-grey-100"
+      @click="emit('next')"
+    >
+      <D9Icon size="1x" icon="chevron-right" />
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { D9Icon } from "@deck9/ui";
+
+defineProps<{
+  currentPage: number;
+  totalPages?: number;
+}>();
+
+const emit = defineEmits<{
+  (e: "prev"): void;
+  (e: "next"): void;
+}>();
+</script>
