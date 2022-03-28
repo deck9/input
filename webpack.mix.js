e0d5ebb79e41191d948c63cbf3e3ff0f9197510a
Philipp Reinking <philipp@deck9.co>
2022-03-28 22:00:34 +0200

    assets transpile only in production build

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,12 @@ const mix = require("laravel-mix");
  */
 
 mix
-    .ts("resources/js/app.ts", "public/js")
-    .ts("resources/js/forms/classic.ts", "public/js")
+    .ts("resources/js/app.ts", "public/js", {
+        transpileOnly: mix.inProduction(),
+    })
+    .ts("resources/js/forms/classic.ts", "public/js", {
+        transpileOnly: mix.inProduction(),
+    })
     .vue()
     .postCss("resources/css/app.css", "public/css", [
         require("postcss-import"),
