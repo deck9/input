3e6e3bc6abcf63337a9918dc8ab8bd5d3b303177
Philipp Reinking <philipp@deck9.co>
2022-04-01 13:29:43 +0200

    remove debug payload on completion page

--- resources/js/forms/classic/layout/CompletionPage.vue
+++ resources/js/forms/classic/layout/CompletionPage.vue
@@ -6,9 +6,6 @@
     <p v-if="store.form?.eoc_text" class="mt-2 text-base leading-6">
       {{ store.form?.eoc_text }}
     </p>
-    <pre class="mt-4 rounded bg-black px-4 py-4 text-xs text-green-600"
-      >{{ JSON.stringify(store.payload) }} </pre
-    >
     <CallToActionButton
       v-if="store.form?.cta_link"
       :href="store.form?.cta_link"
