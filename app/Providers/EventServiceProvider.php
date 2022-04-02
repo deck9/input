7d5dc3433f76a881a3f35571bf2028483a0b76b1
Philipp Reinking <philipp@deck9.co>
2022-04-02 16:58:16 +0200

    disable preview image creation

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -22,7 +22,7 @@ class EventServiceProvider extends ServiceProvider
         ],
 
         FormPublished::class => [
-            CreatePreviewImage::class,
+            // CreatePreviewImage::class,
         ],
     ];
 
