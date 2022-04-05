b6e816e549ba092c4347cf85fa1d2257d46053fa
Philipp Reinking <philipp@deck9.co>
2022-04-05 19:55:25 +0200

    add eyedropper tool to brand color

--- resources/js/components/Factory/Settings/Appearance.vue
+++ resources/js/components/Factory/Settings/Appearance.vue
@@ -14,6 +14,14 @@
               :style="`background-color: ${brandColor}`"
             ></span>
           </label>
+          <button
+            v-if="isSupported"
+            type="button"
+            class="absolute inset-y-2 right-2 h-7 w-7 rounded-full hover:bg-grey-200"
+            @click="openPicker"
+          >
+            <D9Icon size="xs" name="eyedropper" />
+          </button>
         </div>
       </div>
 
@@ -24,14 +32,24 @@
 
 <script setup lang="ts">
 import { useForm } from "@/stores";
-import { D9Button, D9Label, D9Input } from "@deck9/ui";
-import { ref } from "vue";
+import { D9Button, D9Label, D9Input, D9Icon } from "@deck9/ui";
+import { ref, watch } from "vue";
+import { useEyeDropper } from "@vueuse/core";
 import AvatarUpload from "./partials/AvatarUpload.vue";
 
 const store = useForm();
 const isSaving = ref(false);
 
 const brandColor = ref(store?.form?.brand_color);
+const { isSupported, open, sRGBHex } = useEyeDropper();
+
+const openPicker = () => {
+  open();
+};
+
+watch(sRGBHex, (value) => {
+  brandColor.value = value;
+});
 
 const saveOptions = async () => {
   isSaving.value = true;
