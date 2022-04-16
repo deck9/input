dfa4c4387817e49dc52d5d2bea5202eba3db90d6
Philipp Reinking <philipp@deck9.co>
2022-04-16 22:23:50 +0200

    add simple list of responses for results

---
+++ resources/js/components/Factory/Results/ResponseList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="list-inside list-disc">
+    <li
+      class="font-medium leading-6"
+      v-for="response in lastResponses"
+      :key="response.id"
+    >
+      {{ response.value }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+
+const props = defineProps<{
+  action: FormBlockInteractionModel;
+}>();
+
+const lastResponses = computed(() => {
+  return props.action.form_session_responses.slice(-10);
+});
+</script>
