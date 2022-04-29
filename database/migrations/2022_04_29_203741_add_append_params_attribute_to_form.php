71e5abc30a1639e850b74279cb11ed9f699c321e
Philipp Reinking <philipp@deck9.co>
2022-04-29 22:41:32 +0200

    add new attribute cta_append_params to form table

---
+++ database/migrations/2022_04_29_203741_add_append_params_attribute_to_form.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('forms', function (Blueprint $table) {
+            $table->boolean('cta_append_params')->default(false)->after('cta_link');
+        });
+    }
+};
