316ee4d547c00ecedc405d8a96dc8dc7240bbe0a
Philipp Reinking <philipp@deck9.co>
2022-04-29 23:27:41 +0200

    add advanced settings component and add set title for blocks setting

--- resources/js/components/Factory/Results/BlockResultItem.vue
+++ resources/js/components/Factory/Results/BlockResultItem.vue
@@ -4,6 +4,11 @@
     v-if="block.session_count !== 0"
   >
     <div class="px-4 py-5 sm:px-6">
+      <span
+        class="mb-1 inline-block rounded-full bg-blue-50 px-2 py-[2px] text-xs"
+        v-if="block.title"
+        >{{ block.title }}</span
+      >
       <h4 class="text-lg font-medium" v-html="block.message"></h4>
     </div>
     <div class="px-4 py-5 sm:p-6">
