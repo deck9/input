7c7a30569406d77af887727481dc953f1a4f1e1f
Philipp Reinking <philipp@deck9.co>
2022-05-13 14:44:32 +0200

    add foreign keys

---
+++ database/migrations/2022_05_13_115844_add_foreign_key_constraints_to_all_database_tables.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        /** Change index keys to BIGINT */
+        Schema::table('forms', function (Blueprint $table) {
+            $table->bigInteger('id')->unsigned()->change();
+            $table->bigInteger('user_id')->unsigned()->change();
+        });
+
+        Schema::table('form_blocks', function (Blueprint $table) {
+            $table->bigInteger('id')->unsigned()->change();
+            $table->bigInteger('form_id')->unsigned()->change();
+        });
+        Schema::table('form_sessions', function (Blueprint $table) {
+            $table->bigInteger('form_id')->unsigned()->change();
+        });
+
+        Schema::table('form_session_responses', function (Blueprint $table) {
+            $table->bigInteger('id')->unsigned()->change();
+            $table->bigInteger('form_block_id')->unsigned()->change();
+            $table->bigInteger('form_block_interaction_id')->unsigned()->change();
+            $table->bigInteger('form_session_id')->unsigned()->change();
+        });
+
+        /** Remove unused columns */
+        Schema::table('form_block_interactions', function (Blueprint $table) {
+            // $table->dropColumn('has_validation');
+        });
+
+        Schema::table('form_block_interactions', function (Blueprint $table) {
+            // $table->dropColumn('deleted_at');
+        });
+
+        /** Add FOREIGN KEYS */
+        Schema::table('form_blocks', function (Blueprint $table) {
+            $table->foreign('form_id')
+                ->references('id')->on('forms')
+                ->onDelete('CASCADE');
+        });
+
+        Schema::table('form_block_interactions', function (Blueprint $table) {
+            $table->foreign('form_block_id')
+                ->references('id')->on('form_blocks')
+                ->onDelete('CASCADE');
+        });
+
+        Schema::table('form_sessions', function (Blueprint $table) {
+            $table->foreign('form_id')
+                ->references('id')->on('forms')
+                ->onDelete('CASCADE');
+        });
+
+        Schema::table('form_session_responses', function (Blueprint $table) {
+            $table->foreign('form_block_id')
+                ->references('id')->on('form_blocks');
+
+            $table->foreign('form_block_interaction_id')
+                ->references('id')->on('form_block_interactions');
+
+            $table->foreign('form_session_id')
+                ->references('id')->on('form_sessions')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('form_blocks', function (Blueprint $table) {
+            //
+        });
+    }
+};
