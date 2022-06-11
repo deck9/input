71aea8414640e8941db3fa01f4bc0cf79debc462
Philipp Reinking <philipp@deck9.co>
2022-06-12 01:44:35 +0200

    add autoincrement values

---
+++ database/migrations/9999_99_99_999999_set_autoincrement_values.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (config('database.default') === 'sqlite') {
+            DB::statement("UPDATE sqlite_sequence SET seq = 10000 WHERE name = 'forms'");
+            DB::statement("UPDATE sqlite_sequence SET seq = 5000 WHERE name = 'form_blocks'");
+        } else {
+            DB::statement("ALTER TABLE forms AUTO_INCREMENT = 10000;");
+            DB::statement("ALTER TABLE form_blocks AUTO_INCREMENT = 5000;");
+        }
+    }
+};
