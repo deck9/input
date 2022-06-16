150a6ac88f21bba45651b353c8fba0c262d610ce
Philipp Reinking <philipp@deck9.co>
2022-06-16 21:15:17 +0200

    add tests for long input

--- app/Enums/FormBlockInteractionType.php
+++ app/Enums/FormBlockInteractionType.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum FormBlockInteractionType: string
 {
     case input = "input";
+    case textarea = "textarea";
     case button = "button";
     case consent = "consent";
 }
