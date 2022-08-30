3378a6baf7c7f90305fb68b5798c612c1e55f2ae
Philipp Reinking <philipp@deck9.co>
2022-08-30 19:45:02 +0200

    add native input types based on block type

--- resources/js/forms/classic/interactions/InputAction.vue
+++ resources/js/forms/classic/interactions/InputAction.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <label class="sr-only block" :for="action.id"> {{ action.label }}</label>
+    <label class="sr-only block" :for="action.id">{{ action.label }}</label>
     <input
-      type="text"
+      :type="nativeInputType"
       class="block w-full max-w-xs rounded border border-grey-300 bg-white px-3 py-2 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
       :name="block.id"
       :id="action.id"
@@ -17,7 +17,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ComputedRef, inject, onMounted, ref } from "vue";
+import { computed, ComputedRef, inject, onMounted, ref } from "vue";
 import { useConversation } from "@/stores/conversation";
 
 const store = useConversation();
@@ -31,6 +31,28 @@ const disableFocus: ComputedRef<boolean> | undefined = inject("disableFocus");
 const storeValue = (store.currentPayload as FormBlockInteractionPayload)
   ?.payload;
 const inputElement = ref<HTMLInputElement | null>(null);
+
+// get the input element type based on the action type
+const nativeInputType = computed(() => {
+  const action: FormBlockType = props.block.type;
+
+  switch (action) {
+    case "input-number":
+      return "number";
+
+    case "input-email":
+      return "email";
+
+    case "input-link":
+      return "url";
+
+    case "input-phone":
+      return "tel";
+
+    default:
+      return "text";
+  }
+});
 
 onMounted(() => {
   if (!disableFocus?.value) {
