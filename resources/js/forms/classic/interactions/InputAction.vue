1f135bf0d2d7c1fa19d99327b904c16641f339cb
Philipp Reinking <philipp@deck9.co>
2022-12-08 18:02:30 +0100

    use icons to display validation type of short inputs

--- resources/js/forms/classic/interactions/InputAction.vue
+++ resources/js/forms/classic/interactions/InputAction.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="relative">
     <label class="sr-only block" :for="action.id">{{ action.label }}</label>
     <input
       :type="nativeInputType"
       class="block w-full max-w-xs rounded border border-grey-300 bg-white px-3 py-2 focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
+      :class="[useIcon ? 'pl-10' : 'pl-3']"
       :name="block.id"
       :id="action.id"
       :placeholder="action.label || 'Enter text'"
@@ -13,11 +14,19 @@
       @input="onInput"
       v-once
     />
+
+    <div
+      v-if="useIcon"
+      class="absolute inset-y-0 left-3 flex items-center text-grey-700"
+    >
+      <D9Icon :name="useIcon" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, ComputedRef, inject, onMounted, ref } from "vue";
+import { D9Icon } from "@deck9/ui";
 import { useConversation } from "@/stores/conversation";
 
 const store = useConversation();
@@ -51,6 +60,25 @@ const nativeInputType = computed(() => {
 
     default:
       return "text";
+  }
+});
+
+const useIcon = computed(() => {
+  switch (props.block.type) {
+    case "input-number":
+      return "sort-numeric-up";
+
+    case "input-email":
+      return "envelope";
+
+    case "input-link":
+      return "link";
+
+    case "input-phone":
+      return "phone";
+
+    default:
+      return false;
   }
 });
 
