d47cf736b577a103c69336d360d10171bb077331
Philipp Reinking <philipp@deck9.co>
2022-12-15 18:45:58 +0100

    fix inertia resolve function

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,6 @@
 import "@css/app.css";
 import { createApp, h } from "vue";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 import { createPinia } from "pinia";
@@ -11,7 +12,13 @@ const appName =
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: (name) =>
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue")
+        ),
     setup({ el, app, props, plugin }) {
         const vueApp = createApp({ render: () => h(app, props) });
 
