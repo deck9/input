f373c1ae981134a2a0e5889e24e8b67ca0775c02
Philipp Reinking <philipp@deck9.co>
2023-01-15 01:37:52 +0100

    link updates container to changelog page

--- resources/js/components/Dashboard/UpdatePost.vue
+++ resources/js/components/Dashboard/UpdatePost.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="rounded-lg bg-white px-6 py-8 shadow-sm">
+  <component
+    :is="link ? 'a' : 'div'"
+    target="_blank"
+    :href="link"
+    class="block rounded-lg bg-white px-6 py-8 shadow-sm"
+  >
     <h2 class="text-lg font-medium">{{ post.Title }}</h2>
     <span class="text-xs text-grey-500">{{ formattedDate }}</span>
     <p class="mt-2 text-sm leading-6">{{ post.Excerpt }}</p>
-  </div>
+    <D9Button class="mt-2" label="Read more" color="light" />
+  </component>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { D9Button } from "@deck9/ui";
 
 const props = defineProps({
   post: {
     type: Object,
     required: true,
   },
+});
+
+const link = computed<string | null>(() => {
+  if (!props.post.Body || !props.post.Slug) {
+    return null;
+  }
+
+  return `https://getinput.co/changelog/${props.post.Slug}`;
 });
 
 const formattedDate = computed(() => {
