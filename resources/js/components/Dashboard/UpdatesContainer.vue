da4e3430c3e585dcbd5e043706c3c10daaf84499
Philipp Reinking <philipp@deck9.co>
2023-02-24 12:28:04 +0100

    hide loading state for updates container

--- resources/js/components/Dashboard/UpdatesContainer.vue
+++ resources/js/components/Dashboard/UpdatesContainer.vue
@@ -7,14 +7,17 @@
     <div
       class="grid gap-y-4 space-x-2 sm:grid-cols-2 lg:block lg:space-x-0 lg:space-y-4"
     >
-      <template v-if="posts.length === 0">
+      <template v-if="isLoading && posts.length === 0">
         <div class="rounded-lg bg-grey-200 p-4">
           <D9Skeleton class="text-grey-300" footer :lines="4" />
         </div>
         <div class="rounded-lg bg-grey-200 p-4">
-          <D9Skeleton class="text-grey-300" footer :lines="4" />
+          <D9Skeleton class="text-grey-300" footer :lines="3" />
         </div>
       </template>
+
+      <div v-else-if="posts.length === 0">No Updates</div>
+
       <UpdatePost
         v-for="post in posts"
         v-bind="{ post: post.attributes }"
@@ -31,6 +34,9 @@ import { D9Skeleton } from "@deck9/ui";
 import { callChangelog } from "@/api/content";
 
 const posts = ref<any[]>([]);
+const isLoading = ref(true);
+
+const DELAY = 500;
 
 onMounted(async () => {
   try {
@@ -39,9 +45,13 @@ onMounted(async () => {
     // Sets posts.value after a delay to simulate a loading state
     setTimeout(() => {
       posts.value = strapi.data;
-    }, 300);
+    }, DELAY);
   } catch (e) {
-    console.warn("Could not fetch changelog", e);
+    console.warn("Could not fetch changelog");
+  } finally {
+    setTimeout(() => {
+      isLoading.value = false;
+    }, DELAY);
   }
 });
 </script>
