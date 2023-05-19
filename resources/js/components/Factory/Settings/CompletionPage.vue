ca162e9a155e7a9665d80f1f560b072fbd9d064e
Philipp Reinking <philipp@deck9.co>
2023-05-19 09:47:18 +0200

    add switch

--- resources/js/components/Factory/Settings/CompletionPage.vue
+++ resources/js/components/Factory/Settings/CompletionPage.vue
@@ -1,38 +1,12 @@
 <template>
   <form @submit="saveFormSubmitSettings">
-    <div>
-      <h2 class="mb-2 text-base font-bold">General</h2>
+    <CompletionPageType v-model="useCtaRedirect" />
 
-      <div>
-        <div class="mb-4">
-          <D9Label label="Headline" />
-          <D9Input type="text" block v-model="outroHeadline" />
-        </div>
-        <div class="mb-4">
-          <D9Label label="Message" />
-          <D9Input type="text" block v-model="outroMessage" />
-        </div>
-      </div>
-    </div>
-
-    <div class="mt-8">
-      <h2 class="mb-2 flex items-center text-base font-bold">
-        <span class="inline-block">CTA Link</span>
-        <D9Switch class="ml-2" label="" v-model="isCtaOn"></D9Switch>
-      </h2>
-
-      <div
-        class="origin-top transition-all duration-200"
-        :class="
-          isCtaOn
-            ? 'opacity-100'
-            : 'pointer-events-none h-0 scale-y-0 opacity-0 blur-sm'
-        "
-      >
-        <div class="mb-4">
-          <D9Label label="Label" />
-          <D9Input type="text" block :disabled="!isCtaOn" v-model="ctaLabel" />
-        </div>
+    <template v-if="useCtaRedirect">
+      <div class="mt-8">
+        <h2 class="mb-2 flex items-center text-base font-bold">
+          <span class="inline-block">Redirect Settings</span>
+        </h2>
         <div class="mb-4">
           <D9Label label="Link" />
           <D9Input type="url" block :disabled="!isCtaOn" v-model="ctaLink" />
@@ -55,73 +29,142 @@
             offLabel="no"
           />
         </div>
-        <div class="mb-4 flex justify-between">
-          <D9Label label="Automatically Redirect User" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="mt-8">
+        <h2 class="mb-2 text-base font-bold">General</h2>
+
+        <div>
+          <div class="mb-4">
+            <D9Label label="Headline" />
+            <D9Input type="text" block v-model="outroHeadline" />
+          </div>
+          <div class="mb-4">
+            <D9Label label="Message" />
+            <D9Input type="text" block v-model="outroMessage" />
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-8">
+        <h2 class="mb-2 flex items-center text-base font-bold">
+          <span class="inline-block">Call to Action</span>
           <D9Switch
-            label="Automatically Redirect User"
-            v-model="useCtaRedirect"
-            onLabel="yes"
-            offLabel="no"
-          />
+            class="ml-2"
+            label="Activate Call to Action"
+            v-model="isCtaOn"
+          ></D9Switch>
+        </h2>
+
+        <div
+          class="origin-top"
+          :class="
+            isCtaOn
+              ? 'opacity-100'
+              : 'pointer-events-none h-0 scale-y-0 opacity-0 blur-sm'
+          "
+        >
+          <div class="mb-4">
+            <D9Label label="Label" />
+            <D9Input
+              type="text"
+              block
+              :disabled="!isCtaOn"
+              v-model="ctaLabel"
+            />
+          </div>
+          <div class="mb-4">
+            <D9Label label="Link" />
+            <D9Input type="url" block :disabled="!isCtaOn" v-model="ctaLink" />
+          </div>
+          <div class="mb-4 flex justify-between">
+            <D9Label label="Append Incoming Query Parameters" />
+            <D9Switch
+              label="Append Incoming Query Parameters"
+              v-model="ctaAppendParams"
+              onLabel="yes"
+              offLabel="no"
+            />
+          </div>
+          <div class="mb-4 flex justify-between">
+            <D9Label label="Append Session ID as Query Parameter" />
+            <D9Switch
+              label="Append Session ID as Query Parameter"
+              v-model="ctaAppendSessionId"
+              onLabel="yes"
+              offLabel="no"
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="mt-8">
-      <h2 class="mb-2 flex items-center text-base font-bold">
-        <span class="inline-block">Social Links</span>
-        <D9Switch
-          class="ml-2"
-          label="Social Links"
-          v-model="isSocialOn"
-        ></D9Switch>
-      </h2>
+      <div class="mt-8">
+        <h2 class="mb-2 flex items-center text-base font-bold">
+          <span class="inline-block">Social Links</span>
+          <D9Switch
+            class="ml-2"
+            label="Social Links"
+            v-model="isSocialOn"
+          ></D9Switch>
+        </h2>
 
-      <div
-        class="origin-top transition-all duration-200"
-        :class="
-          isSocialOn
-            ? 'opacity-100'
-            : 'pointer-events-none h-0 scale-y-0 opacity-0 blur-sm'
-        "
-      >
-        <div class="mb-4">
-          <D9Label label="Instagram" />
-          <D9Input
-            type="url"
-            block
-            :disabled="!isSocialOn"
-            v-model="instagram"
-          />
-        </div>
-        <div class="mb-4">
-          <D9Label label="Facebook" />
-          <D9Input
-            type="url"
-            block
-            :disabled="!isSocialOn"
-            v-model="facebook"
-          />
-        </div>
-        <div class="mb-4">
-          <D9Label label="Twitter" />
-          <D9Input type="url" block :disabled="!isSocialOn" v-model="twitter" />
-        </div>
-        <div class="mb-4">
-          <D9Label label="LinkedIn" />
-          <D9Input
-            type="url"
-            block
-            :disabled="!isSocialOn"
-            v-model="linkedin"
-          />
-        </div>
-        <div class="mb-4">
-          <D9Label label="Github" />
-          <D9Input type="url" block :disabled="!isSocialOn" v-model="github" />
+        <div
+          class="origin-top"
+          :class="
+            isSocialOn
+              ? 'opacity-100'
+              : 'pointer-events-none h-0 scale-y-0 opacity-0 blur-sm'
+          "
+        >
+          <div class="mb-4">
+            <D9Label label="Instagram" />
+            <D9Input
+              type="url"
+              block
+              :disabled="!isSocialOn"
+              v-model="instagram"
+            />
+          </div>
+          <div class="mb-4">
+            <D9Label label="Facebook" />
+            <D9Input
+              type="url"
+              block
+              :disabled="!isSocialOn"
+              v-model="facebook"
+            />
+          </div>
+          <div class="mb-4">
+            <D9Label label="Twitter" />
+            <D9Input
+              type="url"
+              block
+              :disabled="!isSocialOn"
+              v-model="twitter"
+            />
+          </div>
+          <div class="mb-4">
+            <D9Label label="LinkedIn" />
+            <D9Input
+              type="url"
+              block
+              :disabled="!isSocialOn"
+              v-model="linkedin"
+            />
+          </div>
+          <div class="mb-4">
+            <D9Label label="Github" />
+            <D9Input
+              type="url"
+              block
+              :disabled="!isSocialOn"
+              v-model="github"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </template>
 
     <D9Button
       class="mt-8"
@@ -136,9 +179,12 @@
 import { useForm } from "@/stores";
 import { D9Label, D9Input, D9Switch, D9Button } from "@deck9/ui";
 import { ref } from "vue";
+import CompletionPageType from "@/components/Factory/Settings/partials/CompletionPageType.vue";
 
 const store = useForm();
 const isSaving = ref(false);
+
+console.log(store.form);
 
 const outroHeadline = ref(store.form?.eoc_headline);
 const outroMessage = ref(store.form?.eoc_text);
