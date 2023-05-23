cfc2933b255b8c6b34faa2371308b6a30220559f
Philipp Reinking <philipp@deck9.co>
2023-05-24 00:57:43 +0200

    better action dropdown for form list item

--- resources/js/components/Dashboard/FormListItem.vue
+++ resources/js/components/Dashboard/FormListItem.vue
@@ -4,7 +4,7 @@
     class="transition-sm group relative mb-2 flex rounded bg-white px-8 py-4 text-grey-900 no-underline shadow-sm transition-transform duration-200 hover:no-underline"
     :class="isActive ? 'ring-2' : ''"
   >
-    <div class="flex w-1/2 shrink-0 items-center">
+    <div class="flex w-2/5 shrink-0 items-center">
       <div
         class="relative flex h-12 w-12 shrink-0 items-center justify-center rounded-full p-2"
         :style="`background-color: ${form.brand_color ?? '#232323'};`"
@@ -14,49 +14,13 @@
           :style="`color: ${form.contrast_color}`"
           >{{ form.initials }}</span
         >
-
-        <div
-          :class="[
-            'border-grey-30 absolute -right-[1px] -bottom-[2px] flex h-6 w-6 items-center justify-center rounded-full border border-transparent transition-all duration-150 hover:border-white group-hover:scale-100',
-            isActive ? 'scale-100 border-white' : 'scale-0',
-          ]"
-          :style="`background-color: ${form.brand_color ?? '#232323'}; color: ${
-            form.contrast_color
-          };`"
-        >
-          <D9Menu position="right" use-portal @click="setActive">
-            <D9MenuLink
-              as="a"
-              :href="route('forms.show', { uuid: form.uuid })"
-              target="_blank"
-              class="block w-full text-left"
-              label="View Form"
-            />
-            <D9MenuLink
-              as="a"
-              :href="route('forms.submissions', form.uuid)"
-              class="block w-full text-left"
-              label="Open Submissions"
-            />
-            <D9MenuLink
-              as="a"
-              :href="route('forms.settings', form.uuid)"
-              class="block w-full text-left"
-              label="Open Settings"
-            />
-            <D9MenuLink
-              as="button"
-              type="button"
-              class="block w-full text-left"
-              label="Duplicate Form"
-              @click="duplicateForm"
-            />
-          </D9Menu>
-        </div>
       </div>
       <div class="ml-6 overflow-hidden">
         <h3 class="mb-1 truncate text-base font-bold">
-          <a :href="route('forms.edit', { uuid: form.uuid })">
+          <a
+            class="hover:text-blue-600"
+            :href="route('forms.edit', { uuid: form.uuid })"
+          >
             {{ form.name }}
           </a>
         </h3>
@@ -75,7 +39,7 @@
         </div>
       </div>
     </div>
-    <div class="flex w-1/2 shrink-0 items-center justify-start">
+    <div class="flex w-2/5 shrink-0 items-center justify-start">
       <div class="mx-2 leading-none">
         <div class="font-heading text-xl font-medium">
           {{ form.total_sessions }}
@@ -89,12 +53,56 @@
         <div class="mt-1 text-xs text-grey-500">Completion Rate</div>
       </div>
     </div>
-    <a
-      :href="route('forms.edit', form.uuid)"
-      class="flex w-24 cursor-pointer items-center justify-end group-hover:scale-110 group-hover:text-blue-600"
-    >
-      <D9Icon name="chevron-right" />
-    </a>
+    <div class="flex w-1/5 justify-end">
+      <D9Menu
+        class="flex items-center"
+        position="left"
+        use-portal
+        @click="setActive"
+      >
+        <template #button>
+          <D9Icon
+            :class="[
+              'relative px-4 text-grey-600 transition-all duration-150 group-hover:opacity-100',
+              isActive ? ' opacity-100' : '  opacity-0',
+            ]"
+            name="cog"
+          />
+        </template>
+        <D9MenuLink
+          as="a"
+          :href="route('forms.show', { uuid: form.uuid })"
+          target="_blank"
+          class="block w-full text-left"
+          label="View Form"
+        />
+        <D9MenuLink
+          as="a"
+          :href="route('forms.submissions', form.uuid)"
+          class="block w-full text-left"
+          label="Open Submissions"
+        />
+        <D9MenuLink
+          as="a"
+          :href="route('forms.settings', form.uuid)"
+          class="block w-full text-left"
+          label="Open Settings"
+        />
+        <D9MenuLink
+          as="button"
+          type="button"
+          class="block w-full text-left"
+          label="Duplicate Form"
+          @click="duplicateForm"
+        />
+      </D9Menu>
+      <a
+        :href="route('forms.edit', form.uuid)"
+        class="flex cursor-pointer items-center justify-end transition duration-150 hover:text-blue-600 group-hover:scale-110"
+      >
+        <D9Icon name="chevron-right" />
+      </a>
+    </div>
   </div>
 </template>
 
--- resources/js/components/Dashboard/UpdatePost.vue
+++ resources/js/components/Dashboard/UpdatePost.vue
@@ -1,15 +1,12 @@
 <template>
-  <component
-    :is="link ? 'a' : 'div'"
-    target="_blank"
-    :href="link"
-    class="block rounded-lg bg-white px-6 py-8 shadow-sm"
-  >
+  <div target="_blank" class="block rounded-lg bg-white px-6 py-8 shadow-sm">
     <h2 class="text-lg font-medium">{{ post.Title }}</h2>
     <span class="text-xs text-grey-500">{{ formattedDate }}</span>
     <p class="mt-2 text-sm leading-6">{{ post.Excerpt }}</p>
-    <D9Button class="mt-2" label="Read more" color="light" />
-  </component>
+    <a v-if="link" :href="link" target="_blank">
+      <D9Button class="mt-2" label="Read more" color="light" />
+    </a>
+  </div>
 </template>
 
 <script setup lang="ts">
