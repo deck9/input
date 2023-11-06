a535740c4cf0a519a6bcd64d064568bc4a57a9e4
Philipp Reinking <philipp@deck9.co>
2023-11-06 12:28:56 +0100

    change http client used for webhook and prevent SSRF

--- app/Jobs/CallWebhookJob.php
+++ app/Jobs/CallWebhookJob.php
@@ -2,17 +2,23 @@
 
 namespace App\Jobs;
 
-use App\Http\Resources\FormSessionResource;
+use Exception;
 use App\Models\FormSession;
 use App\Models\FormWebhook;
-use GuzzleHttp\RequestOptions;
 use Illuminate\Bus\Queueable;
+use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Http\Resources\FormSessionResource;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpClient\NoPrivateNetworkHttpClient;
+use Symfony\Component\HttpClient\Exception\TransportException;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CallWebhookJob implements ShouldQueue
 {
@@ -45,28 +51,50 @@ class CallWebhookJob implements ShouldQueue
     {
         $payload = FormSessionResource::make($this->session)->resolve();
 
-        $response = Http::withHeaders(
-            $this->webhook->headers ?? []
-        )->send(
-            $this->webhook->webhook_method,
-            $this->webhook->webhook_url,
-            [
-                RequestOptions::JSON => $payload,
-            ]
-        );
+
+        $client = new NoPrivateNetworkHttpClient(HttpClient::create());
+
+        $client->withOptions([
+            'headers' => $this->webhook->headers ?? []
+        ]);
+
+
+        $response = $client->request($this->webhook->webhook_method, $this->webhook->webhook_url, [
+            'json' => $payload,
+        ]);
+
+        try {
+
+            $status = $response->getStatusCode();
+            $body = $response->getContent();
+            $json = $response->toArray();
+            $headers = $response->getHeaders();
+        } catch (HttpExceptionInterface $e) {
+            $errorResponse = $e->getResponse();
+
+            $status = $errorResponse->getStatusCode(false);
+            $body = $errorResponse->getContent(false);
+            $json = $errorResponse->toArray(false);
+            $headers = $errorResponse->getHeaders(false);
+        } catch (Exception $e) {
+            $status = 500;
+            $body = $e->getMessage();
+            $json = ['error' => $e->getMessage()];
+            $headers = [];
+        }
 
         Log::debug('Webhook response', [
-            'status' => $response->status(),
-            'body' => $response->body(),
-            'json' => $response->json(),
-            'headers' => $response->headers(),
+            'status' => $status,
+            'body' => $body,
+            'json' => $json,
+            'headers' => $headers,
         ]);
 
         $this->session->webhooks()->updateOrCreate([
             'form_webhook_id' => $this->webhook->id,
         ], [
-            'status' => $response->status(),
-            'response' => $response->json() ?? $response->body(),
+            'status' => $status,
+            'response' => $json ?? $body,
             'tries' => $this->session->webhooks()->where('form_webhook_id', $this->webhook->id)->count() + 1,
         ]);
     }
