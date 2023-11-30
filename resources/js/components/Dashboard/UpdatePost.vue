3d2615ba6207db3200b9ec3678e009b94e34bbf3
Philipp Reinking <philipp@deck9.co>
2023-11-30 12:41:37 +0200

    add new badge to update item

--- resources/js/components/Dashboard/UpdatePost.vue
+++ resources/js/components/Dashboard/UpdatePost.vue
@@ -1,10 +1,21 @@
 <template>
-  <div target="_blank" class="block rounded-lg bg-white px-6 py-8 shadow-sm">
+  <div class="block rounded-lg bg-white px-6 py-8 shadow-sm">
     <h2 class="text-lg font-medium">{{ post.Title }}</h2>
     <span class="text-xs text-grey-500">{{ formattedDate }}</span>
     <p class="mt-2 text-sm leading-6">{{ post.Excerpt }}</p>
-    <a v-if="link" :href="link" target="_blank">
+    <a
+      class="relative"
+      v-if="link"
+      @click="trackRead"
+      :href="link"
+      target="_blank"
+    >
       <D9Button class="mt-2" label="Read more" color="light" />
+      <span
+        v-if="!isRead"
+        class="bg-blue-300 rounded px-2 py-px bg-gradient-to-bl from-purple-600 via-teal-400 to-blue-400 font-bold text-white uppercase text-xs top-0 absolute right-100 -mt-2 -ml-2"
+        >New</span
+      >
     </a>
   </div>
 </template>
@@ -12,6 +23,9 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { D9Button } from "@deck9/ui";
+import { useStorage } from "@vueuse/core";
+
+const state = useStorage<Array<string>>("readPosts", []);
 
 const props = defineProps({
   post: {
@@ -27,6 +41,18 @@ const link = computed<string | null>(() => {
 
   return `https://getinput.co/changelog/${props.post.Slug}`;
 });
+
+const isRead = computed(() => {
+  return state.value.includes(props.post.Slug);
+});
+
+const trackRead = () => {
+  if (isRead.value) {
+    return;
+  }
+
+  state.value.push(props.post.Slug);
+};
 
 const formattedDate = computed(() => {
   if (!props.post.PublishedDate) {
