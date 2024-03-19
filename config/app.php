53ea1aa4439accfd8676dedb9431470d260f5afd
Philipp Reinking <philipp@deck9.co>
2024-03-19 15:33:02 +0100

    change default app url to value that is used in docker image

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost:8500'),
+    'url' => env('APP_URL', 'http://localhost:8080'),
 
     'asset_url' => env('ASSET_URL', null),
     'mix_url' => env('ASSET_URL', null),
