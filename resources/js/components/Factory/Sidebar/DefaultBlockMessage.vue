17868c36b37445954db7f56280f69a5229e62ac9
Philipp Reinking <philipp@deck9.co>
2024-09-19 14:12:36 +0200

    improve block message rendering

---
+++ resources/js/components/Factory/Sidebar/DefaultBlockMessage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="form-message-prose conversation-theme mb-4"
+    :class="[{ 'max-h-44 overflow-hidden': isCollapsed }]"
+    v-html="content"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  content: string;
+}>();
+</script>
